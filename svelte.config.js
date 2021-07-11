4a179cb2ebdaba0e9867e9015100089048e530f1
Hugo Tunius <h@tunius.se>
2021-07-11 17:35:22 +0100

    Run prettier

--- svelte.config.js
+++ svelte.config.js
@@ -3,25 +3,25 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess({
-    scss: {
-      includePaths: ['node_modules/normalize-scss/sass/']
-    }
-  }),
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess({
+		scss: {
+			includePaths: ['node_modules/normalize-scss/sass/']
+		}
+	}),
 
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
-    ssr: false,
-    adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    })
-  }
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		ssr: false,
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
+	}
 };
 
 export default config;
